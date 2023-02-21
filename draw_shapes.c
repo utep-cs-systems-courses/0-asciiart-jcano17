db8f2259b9b5b199eb8a22037ee3246ef4a78a41
“john <jcano17@miners.utep.edu>
2023-02-21 10:17:15 -0700

    draw_shapes.c

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,30 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+//prints arrow of specified height whose left edge is at col leftCol.
+void print_arrow(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+  int i, j;
+  
+  int endCol = leftCol + size ;
+  leftCol += size/2 + 1 ;
+  if (size%2==0){
+      endCol += size/2;
+  }
+  else{
+      endCol += size/2 +1;
+  }
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol; col++) putchar(' ');
+    for (       ; col < endCol;  col++) putchar('*');
+    putchar('\n');
+  }
+}
